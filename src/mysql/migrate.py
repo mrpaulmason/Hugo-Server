db12a75539326b80d82802d79db1f490bc7cfc52
Ryan Waliany <rwaliany@gmail.com>
2012-08-21 01:23:15 -0700

    Working migration script for Hugo.

--- src/mysql/migrate.py
+++ src/mysql/migrate.py
@@ -42,37 +42,79 @@ def performMigration(v):
     if cur.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'hugo_%s'" % (env.lower())) == 0:
         if v == 0:
             print "Running initial migration..."
+            try:
+                cur.execute("CREATE DATABASE hugo_%s" % (env.lower()))
+                files = os.listdir(str(v))            
+                for file in files:
+                    process = Popen('mysql %s -h%s -P%d -u%s -p%s' % ("hugo_%s" % (env.lower()), dbhost, dbport, "hugo", "Huego415"),
+                                    stdout=PIPE, stdin=PIPE, shell=True)
+                    output = process.communicate('source ' + "%d/%s" % (v, file))[0]
+                    print output
+                print "Executing: %s" % ("INSERT INTO hugo_%s.version_info (version) VALUES(0)" % (env.lower()))
+                cur.execute("INSERT INTO hugo_%s.version_info (version) VALUES(0)" % (env.lower()))
+                conn.commit()
+            except:
+                cur.execute("DROP DATABASE hugo_%s" % (env.lower()))
+                print "Error running migration"
+                sys.exit(2)
+            print "Migration complete!"
         else:
             print "Error: please run initial migration since the database doesn't exist"
             sys.exit(2)            
-    elif cur.execute("SELECT max(version) from hugo_%s.version_info") == 1:
+    elif cur.execute("SELECT max(version) from hugo_%s.version_info" % (env.lower())) == 1:
         lastVersion = cur.fetchone()[0]
-        if lastVersion != v-1:
+        if lastVersion == v:
+            print "Error: database is already up to date!"
+        elif lastVersion != v-1:
             print "Error: please run migrations in order, the current database version is %d" % lastVersion
             sys.exit(2)
         else:
             print "Running upgrade to %d." % v
+            try:
+                cur.execute("CREATE DATABASE hugo_%s_tmp" % (env.lower()))
                 
-    # use cursor. 
-    
-    code.interact(local=locals())
-"""
-    conn = MySQLdb.connect (host = "localhost",
-                               user = "testuser",
-                               passwd = "testpass",
-                               db = "test")
-       cursor = conn.cursor ()
-       cursor.execute ("SELECT VERSION()")
-       row = cursor.fetchone ()
-       print "server version:", row[0]
-       cursor.close ()
-       conn.close ()
+                cur.execute("SHOW TABLES FROM hugo_%s" % (env.lower()))
+                
+                results = cur.fetchall()
+                for item in results:
+                    cur.execute("CREATE TABLE hugo_%s_tmp.%s like hugo_%s.%s" % (env.lower(), item[0], env.lower(), item[0]))
+                    cur.execute("INSERT INTO hugo_%s_tmp.%s SELECT * FROM hugo_%s.%s" % (env.lower(), item[0], env.lower(), item[0]))                                
+                    
+                conn.commit()
+                
+                
+                files = os.listdir(str(v))            
+                for file in files:
+                    process = Popen('mysql %s -h%s -P%d -u%s -p%s' % ("hugo_%s_tmp" % (env.lower()), dbhost, dbport, "hugo", "Huego415"),
+                                    stdout=PIPE, stdin=PIPE, shell=True)
+                    output = process.communicate('source ' + "%d/%s" % (v, file))[0]
+                    print output
+                print "Executing: %s" % ("INSERT INTO hugo_%s_tmp.version_info (version) VALUES(0)" % (env.lower()))
+                cur.execute("INSERT INTO hugo_%s_tmp.version_info (version) VALUES(0)" % (env.lower()))
+                conn.commit()
+                
+                # If we made it this far without an error, we can run the commands on our primary database
+                print "Migration complete on backup database, running migration on real database"
+                files = os.listdir(str(v))            
+                for file in files:
+                    process = Popen('mysql %s -h%s -P%d -u%s -p%s' % ("hugo_%s" % (env.lower()), dbhost, dbport, "hugo", "Huego415"),
+                                    stdout=PIPE, stdin=PIPE, shell=True)
+                    output = process.communicate('source ' + "%d/%s" % (v, file))[0]
+                    print output
+                print "Executing: %s" % ("INSERT INTO hugo_%s.version_info (version) VALUES(0)" % (env.lower()))
+                cur.execute("INSERT INTO hugo_%s.version_info (version) VALUES(%d)" % (env.lower(), v))
+                conn.commit()
 
-    process = Popen('mysql %s -u%s -p%s' % ("hugo_%s" % (env.lower()), "hugo", "Huego415"),
-                    stdout=PIPE, stdin=PIPE, shell=True)
-    output = process.communicate('source ' + "%d/%s.sql" % (v, "categories"))[0]
-    print output
-"""
+                print "Cleaning up!"
+                cur.execute("DROP DATABASE hugo_%s_tmp" % (env.lower()))                
+                
+            except:
+                cur.execute("DROP DATABASE hugo_%s_tmp" % (env.lower()))
+                print "Error running migration"
+                sys.exit(2)
+            print "Migration complete!"
+                
+    
 
 def usage():
     print "migrate.py [--env=local|staging|production] version_number"
