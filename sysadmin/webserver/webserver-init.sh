044fc123377f81dde95a6d8f383ded3d1c8ad5b0
Ryan Waliany <rwaliany@gmail.com>
2012-08-24 14:04:03 -0700

    hugo

--- sysadmin/webserver/webserver-init.sh
+++ sysadmin/webserver/webserver-init.sh
@@ -24,6 +24,8 @@ curl http://python-distribute.org/distribute_setup.py | python
 curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
 pip install cloud
 
+pip install --upgrade boto
+
 echo "Copying key" >> /etc/webserver-init.touchdown
 
 cat <<EOF > /root/hugo.pem
